50506e70e67385b859828ef2cf2286acfa5da408
장영완 <ywjang2@gmail.com>
2021-10-19 18:40:24 +0900

    ALLOWED_HOSTS변경

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'ls&1oied3t*np!^y6664m!j2-l%ie=8g$w-bjk_pjryzd#3y6b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.36.105.222']
 
 
 # Application definition
