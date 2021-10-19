73c329cdd1d38a5400f5e595e9d79e61cbb09b15
장영완 <ywjang2@gmail.com>
2021-10-19 12:31:55 +0900

    Host Modify

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'ls&1oied3t*np!^y6664m!j2-l%ie=8g$w-bjk_pjryzd#3y6b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.124.13.147']
+ALLOWED_HOSTS = []
 
 
 # Application definition
